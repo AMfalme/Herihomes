f03a8757ccddfa7ea2c8e63bc6028f0bf0276bfe
GriffinMfalme <gmfalme@mizizi.io>
2019-10-03 15:18:44 +0300

    enable editing of homepage sections such as the testimonials section and the homepage-portfolio page that will link to specific pages

--- home/models.py
+++ home/models.py
@@ -12,11 +12,17 @@ from django import forms
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    def get_context(self, request):
+        # Update context to include only published portolio projects, ordered by reverse-chron
+        context = super().get_context(request)
+        portfolios = self.get_children().live().order_by('-first_published_at')
+        context['portfolios'] = portfolios
+        return context
     
-
     content_panels = Page.content_panels + [
     FieldPanel('body', classname="full"),
     InlinePanel('Sliders', label="Slider section"),
+    InlinePanel('Testimonials', label="Testimonials")
 
     ]
 
@@ -36,4 +42,48 @@ class SliderInfo(Orderable):
     FieldPanel('intro_pitch'),
     FieldPanel('pitch'),
     ImageChooserPanel('image'),
+    ]
+
+
+
+class PortfolioPage(Page):
+    body = RichTextField(blank=True)
+    location = models.TextField(blank=True)
+    mosque = models.TextField(blank=True)
+    beds = models.TextField(blank=True)
+    lifts =  models.BooleanField(default=False)
+    baths = models.TextField(blank=True)
+    garage = models.TextField(default=True)
+    floors = models.TextField(blank=True)
+    project_image = models.ForeignKey(
+    'wagtailimages.Image', on_delete=models.SET_NULL,null= True, related_name='+'
+    )
+    content_panels = Page.content_panels + [
+    FieldPanel('body'),
+    FieldPanel('location'),
+    FieldPanel('mosque'),
+    FieldPanel('beds'),
+    FieldPanel('lifts'),
+    FieldPanel('baths'),
+    FieldPanel('garage'),
+    FieldPanel('floors'),
+    ImageChooserPanel('project_image'),
+    ]
+
+class Testimonials(Orderable):
+    Page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name = "Testimonials")
+    client_quote = models.TextField(blank=True)
+    client_testimony =RichTextField()
+    client_name = models.TextField(blank=True)
+    client_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+        )
+    panels =  [
+    FieldPanel('client_quote'),
+    FieldPanel('client_testimony'),
+    FieldPanel('client_name'),
+    ImageChooserPanel('client_image')
     ]
